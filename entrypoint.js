30f070ff1301da85a3dbe071f18b15f91d1b16bd
Avner Cohen <israbirding@gmail.com>
2018-12-12 11:15:15 +0200

    Initial commit

---
+++ entrypoint.js
@@ -0,0 +1,66 @@
+var childProcess = require('child_process')
+  , net = require('net')
+  , _startFontelloTimeout = parseInt(process.env.STARTUP_TIMEOUT, 10)
+  , startFontelloTimeout = isNaN(_startFontelloTimeout) ? 120e3 : _startFontelloTimeout
+  , fontelloTimeout = setTimeout(function() { process.exit(2) }, startFontelloTimeout)
+
+fontelloTimeout.unref()
+
+var setupLogging = function(source) {
+  source.stdout.pipe(process.stdout)
+  source.stderr.pipe(process.stderr)
+}
+
+var mongodRepair = childProcess.spawn('mongod', ['--repair'])
+  , mongod = null
+  , fontello = null
+
+setupLogging(mongodRepair)
+
+mongodRepair.once('exit', function startMongod() {
+  mongodRepair = null
+  mongod = childProcess.spawn('mongod', ['--nojournal', '--noprealloc'])
+  setupLogging(mongod)
+})
+
+var cleanupFunc = function(signal) {
+  return function() {
+    console.log('Cleaning up because ' + signal + ' received.')
+    var killTimeout = setTimeout(function() {
+      mongodRepair && mongodRepair.kill('SIGKILL')
+      mongo && mongod.kill('SIGKILL')
+      fontello && fontello.kill(signal)
+      setTimeout(function() { process.exit(1) }, 50)
+    }, 15e3)
+    killTimeout.unref()
+    mongodRepair && mongodRepair.kill(signal)
+    mongod && mongod.kill(signal)
+    fontello && fontello.kill(signal)
+  }
+}
+var startFontello = function() {
+  fontello = childProcess.spawn('./server.js')
+  setupLogging(fontello)
+}
+
+console.log('Waiting for mongo.')
+
+;(function waitForFontello() {
+  var fontelloConnection = net.createConnection({ port: 27017 })
+
+  fontelloConnection.on('connect', function() {
+    console.log('Starting fontello.')
+    clearTimeout(fontelloTimeout)
+    startFontello()
+    this.end()
+  })
+  fontelloConnection.on('error', function() {
+    setTimeout(waitForFontello, 100)
+  })
+})()
+
+;[ 'SIGINT', 'SIGTERM' ].forEach( function(signal) { process.on(signal, cleanupFunc(signal)) })
+
+process.on('exit', function(code) {
+  console.log('Manager process exiting')
+})
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Fontello Server
+
+Fontello server in a docker - https://github.com/fontello/fontello
+
+## Getting Started
+
+docker pull avnerner/fontello-server
+
+### Prerequisities
+
+
+In order to run this container you'll need docker installed.
+
+* [Windows](https://docs.docker.com/windows/started)
+* [OS X](https://docs.docker.com/mac/started/)
+* [Linux](https://docs.docker.com/linux/started/)
+
+### Usage
+
+```
+docker run -d -p 3000:3000 fontello_server
+```
+
+## Built With
+
+* ubuntu 14.04
+* node 8.14.0
+* Fontello 7.1.0
+
+## Find Us
+
+* [GitHub](https://github.com/AvnerCohen)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Acknowledgments
+
+* Thanks to https://github.com/ixti and https://github.com/puzrin for making fontello!
